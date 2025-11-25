1ca9339054b1d2096b0528a7d1658ae29bf52b90
hispin <hag.hispin12@gmail.com>
2025-11-25 18:13:14 +0200

    RATE TUB

--- app/src/main/java/com/example/mathprojectavishaigozland/LogInActivity.java
+++ app/src/main/java/com/example/mathprojectavishaigozland/LogInActivity.java
@@ -1,6 +1,7 @@
  package com.example.mathprojectavishaigozland;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -31,21 +32,30 @@ public class LogInActivity extends AppCompatActivity {
         });
         initViews();
         createClickListener();
+
+        SharedPreferences sh = getSharedPreferences("MyShardPref", MODE_PRIVATE);
+        String s1 = sh.getString("name","");
+        ED_UserName.setText(s1);
     }
 
     private void initViews() {
         Btn_NextPage=findViewById(R.id.NextPage);
         ED_UserName=findViewById(R.id.UserName);
     }
-    public void createClickListener(){
+    public void createClickListener() {
         Btn_NextPage.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
+                SharedPreferences sharedPreferences = getSharedPreferences("MyShardPref", MODE_PRIVATE);
+                SharedPreferences.Editor myEdit = sharedPreferences.edit();
+                myEdit.putString("name", ED_UserName.getText().toString());
+                myEdit.apply();
+
                 Intent intent = new Intent(LogInActivity.this, MainActivity.class);
-                intent.putExtra("userName",ED_UserName.getText().toString());
+                intent.putExtra("userName", ED_UserName.getText().toString());
                 startActivity(intent);
             }
+        });
 
-            });
-        }
+    }
 }
